e9f8ba6ee6c2db61d62cb362598e391986a75b4e
jah3 <isecov9@gmail.com>
2024-11-06 09:19:16 +0200

    RabbitMQConfig class bean creation

--- integration-notification-service/src/main/java/md/maib/integrationnotificationservice/config/RabbitMQConfig.java
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/config/RabbitMQConfig.java
@@ -1,7 +1,14 @@
 package md.maib.integrationnotificationservice.config;
 
+import org.springframework.amqp.core.Queue;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Bean
+    public Queue notificationsQueue() {
+        return new Queue("notifications");
+    }
 }
