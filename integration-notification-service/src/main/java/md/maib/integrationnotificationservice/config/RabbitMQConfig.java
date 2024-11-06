ebf2f9b92edd31349e5d7fa4072534f2bed3af63
jah3 <isecov9@gmail.com>
2024-11-06 08:57:34 +0200

    added new classes

---
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/config/RabbitMQConfig.java
@@ -0,0 +1,7 @@
+package md.maib.integrationnotificationservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+}
---
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package md.maib.integrationnotificationservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+}
