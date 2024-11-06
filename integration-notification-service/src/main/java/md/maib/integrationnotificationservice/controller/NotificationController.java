fbfaec1624da52b0f4be7119dff24fb856b62eb5
jah3 <isecov9@gmail.com>
2024-11-06 09:24:09 +0200

    Integration Post Controller Created

--- integration-notification-service/src/main/java/md/maib/integrationnotificationservice/controller/NotificationController.java
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/controller/NotificationController.java
@@ -1,6 +1,11 @@
 package md.maib.integrationnotificationservice.controller;
 
+import md.maib.integrationnotificationservice.model.EventModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/notifications")
 public class NotificationController {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public NotificationController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendNotification(@RequestBody EventModel event) {
+        rabbitTemplate.convertAndSend("notifications", event);
+        return "Notification sent to RabbitMQ successfully!";
+    }
 }
