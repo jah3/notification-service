fab85f8495c68f5b95c6580c074782f0dd50b68a
Mihai <potoracmihai74@gmail.com>
2024-11-07 14:26:19 +0200

    Update RabbitMQConfig

--- integration-notification-service/src/main/java/md/maib/integrationnotificationservice/config/RabbitMQConfig.java
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/config/RabbitMQConfig.java
@@ -2,6 +2,9 @@ package md.maib.integrationnotificationservice.config;
 
 import org.springframework.amqp.core.Queue;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +13,12 @@ public class RabbitMQConfig {
     @Bean
     public Queue notificationsQueue() {
         return new Queue("notifications");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 }
