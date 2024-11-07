f14fd123724931aa951512f8a205af44756a8e92
Mihai <potoracmihai74@gmail.com>
2024-11-07 14:47:19 +0200

    Update mailnotification RabbitMQConfig

--- mail-notification-service/src/main/java/md/maib/mailnotificationservice/config/RabbitMQConfig.java
+++ mail-notification-service/src/main/java/md/maib/mailnotificationservice/config/RabbitMQConfig.java
@@ -1,7 +1,15 @@
 package md.maib.mailnotificationservice.config;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
