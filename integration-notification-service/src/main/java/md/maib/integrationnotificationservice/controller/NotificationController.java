a8a71feb6c5340dccf06c09e954112628c159993
Mihai <potoracmihai74@gmail.com>
2024-11-07 14:38:55 +0200

    Update NotificationController

--- integration-notification-service/src/main/java/md/maib/integrationnotificationservice/controller/NotificationController.java
+++ integration-notification-service/src/main/java/md/maib/integrationnotificationservice/controller/NotificationController.java
@@ -4,10 +4,7 @@ import md.maib.integrationnotificationservice.model.EventModel;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RestController
@@ -21,8 +18,8 @@ public class NotificationController {
     }
 
 
-    @PostMapping(value = "/send", consumes = "application/x-www-form-urlencoded")
-    public String sendNotificationFromForm(@ModelAttribute EventModel event) {
+    @PostMapping("/send")
+    public String sendNotification(@RequestBody EventModel event) {
         rabbitTemplate.convertAndSend("notifications", event);
         return "Notification sent to RabbitMQ successfully!";
     }
